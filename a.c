d5ce8c85d3124f78e3dba39a898d632c3c061a33
sanghun4996 <sanghun9376@naver.com>
2015-09-30 18:45:26 +0900

    Create a.c

---
+++ a.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+
+	scanf("%d%d",&a,&b);
+
+	printf("%d\n",a);
+	return 0;
+}
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	int result;
+	
+	scanf("%d %d",&a, &b);
+
+	result=a*b;
+	printf("%d \n", result);
+
+	return 0;
+}
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	int min;
+	int output;
+	scanf("%d", &min);
+	
+	output=min*60;
+	printf("%d minutes is %d seconds.",min, output);
+	return 0;
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b;
+	scanf("%d %d", &a, &b);
+
+	printf("%d %d",)
